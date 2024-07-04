d9348fd796b86fbe2059b75c2b21bf4270775d17
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-07-04 17:08:27 +0100

    added api domain

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 // Configuration
 cloudinary.config({
-  cloud_name: "ddwmcikmw",
-  api_key: "283347834735144",
-  api_secret: "IX9m0c0tfDAkQvP7DNO5uP_VJ0c", // Click 'View Credentials' below to copy your API secret
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET, // Click 'View Credentials' below to copy your API secret
 });
 
 export default cloudinary;
