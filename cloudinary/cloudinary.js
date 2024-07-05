5f596a9ce083d28ff675587cb434ca84c5f4a68c
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-07-05 23:19:14 +0100

    cloudinary fix

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -6,7 +6,7 @@ dotenv.config();
 cloudinary.config({
   cloud_name: 'ddwmcikmw',
   api_key: '283347834735144',
-  api_secret: 'X9m0c0tfDAkQvP7DNO5uP_VJ0c ', // Click 'View Credentials' below to copy your API secret
+  api_secret: 'IX9m0c0tfDAkQvP7DNO5uP_VJ0c', // Click 'View Credentials' below to copy your API secret
 });
 
 export default cloudinary;
