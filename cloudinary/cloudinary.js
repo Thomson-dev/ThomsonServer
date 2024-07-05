28f1e2ba298e77cd81e8e5c48123287483bf2eda
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-07-05 22:13:38 +0100

    new change

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 // Configuration
 cloudinary.config({
-  cloud_name: 'IX9m0c0tfDAkQvP7DNO5uP_VJ0c',
+  cloud_name: 'ddwmcikmw',
   api_key: '283347834735144',
   api_secret: 'X9m0c0tfDAkQvP7DNO5uP_VJ0c ', // Click 'View Credentials' below to copy your API secret
 });
